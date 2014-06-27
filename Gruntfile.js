55b29cfd2dead58de7debe056a63e45adb1e8f58
Richard Gustin <richard.gustin86@gmail.com>
2014-06-28 01:52:16 +1000

    chore(cleanup): Removed un-needed code from the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,8 @@
 
 module.exports = function( grunt ) {
     return [{
-        exec: {
-            rebase: {
-                cmd: "NODE_PATH=./lib/:./modules/; node modules/clever-orm/bin/rebase.js"
-            },
-            seed: {
-                cmd: "NODE_PATH=./lib/:./modules/; node modules/clever-orm/bin/seedModels.js"
-            }
-        }
+        
     }, function( grunt ) {
-        // Register each command
-        grunt.registerTask( 'db:rebase', [ 'exec:rebase' ] );
-        grunt.registerTask( 'db:seed', [ 'exec:seed' ] );
 
-        // Register db command (runs one after the other)
-        grunt.registerTask( 'db', [ 'db:rebase', 'db:seed' ] );
     }];
 };
