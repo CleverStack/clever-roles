a0d917de3d9573bae067af254ed09457a77a1190
Richard Gustin <richard.gustin86@gmail.com>
2014-07-21 15:24:30 +1000

    feat(events): Implemented preQuery event for Roles

--- module.js
+++ module.js
@@ -1,3 +1,28 @@
 module.exports = ( require( 'classes' ).Module ).extend({
+    preRoute: function() {
+        var injector        = require( 'injector' )
+          , UserModel       = injector.getInstance( 'UserModel' )
+          , RoleModel       = injector.getInstance( 'RoleModel' )
+          , PermissionModel = injector.getInstance( 'PermissionModel' );
 
+        UserModel.on( 'preQuery', function( options ) {
+            console.log( 'UserModel:preQuery()');
+            if ( typeof options.include === 'undefined' ) {
+                options.include = [];
+            }
+            if ( options.include.indexOf( RoleModel._model ) === -1 ) {
+                options.include.push( RoleModel._model );
+            }
+        });
+
+        RoleModel.on( 'preQuery', function( options ) {
+            console.log( 'RoleModel:preQuery()');
+            if ( typeof options.include === 'undefined' ) {
+                options.include = [];
+            }
+            if ( options.include.indexOf( PermissionModel._model ) === -1 ) {
+                options.include.push( PermissionModel._model );
+            }
+        });
+    }
 });
