fb815cf819c4fe50e45a0f8251a04410dc369cfa
Richard Gustin <richard.gustin86@gmail.com>
2014-07-21 15:26:26 +1000

    refactor(service): Reimplemented the hasPermission function in the PermissionService

--- services/PermissionService.js
+++ services/PermissionService.js
@@ -4,6 +4,56 @@ module.exports = function ( Service, PermissionModel, RoleModel ) {
     return Service.extend({
         model: PermissionModel,
 
+        hasPermissions: function( user, requiredPermissions, matcher ) {
+            var that = this;
+
+            return function( req, res, next ) {
+                var user = req.session.passport.user;
+
+                async.waterfall(
+                    [
+                        function isAuthenticated( callback ) {
+                            callback( req.isAuthenticated() ? null : 'User is not authenticated!' );
+                        },
+
+                        function lazyLoadRoles( callback ) {
+                            if ( !user.role || !user.role.permissions ) {
+                                RoleService
+                                    .getRoleWithPerms( user.RoleId )
+                                    .then( function( role ) {
+                                        if ( role === null || !( role instanceof RoleModel ) ) {
+                                            callback( 'Logged in user does not have a role with the "' + requiredPermission + '" permission.' );
+                                        } else {
+                                            user.role = role;
+                                            callback( null );
+                                        }
+                                    })
+                                    .catch( callback );
+                            } else {
+                                callback( null );
+                            }
+                        },
+
+                        function userRoleHasPermission( callback ) {
+                            if ( _.findWhere( user.role.permissions, { action: requiredPermission } ) !== undefined ) {
+                                callback( null );
+                            } else {
+                                callback( 'Logged in user does not have ' + requiredPermission + ' permission.' );
+                            }
+                        }
+                    ],
+                    function( err ) {
+                        if ( err === null ) {
+                            next();
+                        } else {
+                            return res.send( 403, { statusCode: 403, message: err } );
+                        }
+                    }
+
+                );
+            }
+        },
+
         hasPermissions: function ( req, permissions, booleanLogic ) {
             return new Promise( function( resolve, reject ) {
                 permissions = Array.isArray ( permissions ) ? permissions : [ permissions ];
