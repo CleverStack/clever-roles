d7c22268157923d1a44677b3b9fb8d64f52cd81e
Richard <richard.gustin86@gmail.com>
2014-11-24 23:54:45 +1000

    fix(queryIncludes): Added in missing includes for RoleModel and PermissionModel

--- module.js
+++ module.js
@@ -25,6 +25,17 @@ module.exports = require( 'classes' ).Module.extend({
             if ( options.include.indexOf( { model: PermissionModel._model } ) === -1 ) {
                 options.include.push( { model: PermissionModel._model } );
             }
+
+            if ( options.include.indexOf( { model: UserModel._model } ) === -1 ) {
+                options.include.push( { model: UserModel._model } );
+            }
+        });
+
+        PermissionModel.on( 'preQuery', function( options ) {
+            options.include = typeof options.include !== 'undefined' ? options.include : [];
+            if ( options.include.indexOf( { model: RoleModel._model } ) === -1 ) {
+                options.include.push( { model: RoleModel._model } );
+            }
         });
     }
 });
